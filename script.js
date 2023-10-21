40bd6dc28f759dc9a9bd8c3e3ebee630334aba35
SBsKingdom <hossainshiaib@gmail.com>
2023-10-21 22:23:10 +0600

    uyt

--- script.js
+++ script.js
@@ -13,26 +13,60 @@ setInterval(Slider , 2000);
 // Skill > Circle --------------//-----------///------------///
 
 const circle = document.querySelectorAll('.circle');
-circle.forEach(elem=>{
-    var dots = elem.getAttribute("data-dots");
-    var marked = elem.getAttribute("data-percent");
-    var parcent = Math.floor(dots*marked/100)
-    var points = "";
-    var rotate = 360 / dots;
-
-    for (let i = 0; i < dots; i++) {
-        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg;"></div>`
-        
-    }
-
-    elem.innerHTML = points;
-
-    const pointMarked = elem.querySelectorAll('.points');
-    for (let s = 0; s < parcent; s++) {
-        pointMarked[s].classList.add('marked');
-        console.log(pointMarked[s])
-    }
-
-})
+const skill = document.querySelectorAll(".skill")
 
 ////////////////////////////////////////////
+
+
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function handleScroll() {
+    circle.forEach(element => {
+        if (isElementInViewport(element)) {
+            circle.forEach(elem=>{
+                var dots = elem.getAttribute("data-dots");
+                var marked = elem.getAttribute("data-percent");
+                var parcent = Math.floor(dots*marked/100)
+                var points = "";
+                var rotate = 360 / dots;
+            
+                for (let i = 0; i < dots; i++) {
+                    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg;"></div>`
+                    
+                }
+            
+                elem.innerHTML = points;
+            
+                const pointMarked = elem.querySelectorAll('.points');
+                for (let s = 0; s < parcent; s++) {
+                    pointMarked[s].classList.add('marked');
+                    console.log(pointMarked[s])
+                }
+            
+            })
+        }
+    });
+}
+
+const bar = document.querySelectorAll(".fillBar");
+function handleScroll2() {
+    bar.forEach(element=> {
+        if (isElementInViewport(element)) {
+            element.classList.add('run')
+
+        }
+    })
+}
+
+window.addEventListener("scroll", handleScroll);
+window.addEventListener("scroll", handleScroll2);
+
+
